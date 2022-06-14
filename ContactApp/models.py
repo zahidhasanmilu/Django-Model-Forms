4748e81551698175b2b3927814aa268e8d5996bd
Minion Milu <Zahidhasan.milu@gmail.com>
2022-06-14 23:14:08 +0600

    Contact Model create

---
+++ ContactApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    address = models.TextField()
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
